e1bb51683005d4dc8a29a8c126eaefafe05ec1da
flaskur <flaskur@gmail.com>
2021-03-08 21:36:04 -0500

    initial commit

---
+++ components/navbar.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Navbar = ({text}: NavbarProps) => {
+	
+	return (
+		<div>
+			<h1>navbar thingy</h1>
+			<h2>simple {text}</h2>
+		</div>
+	);
+};
+
+export default Navbar;
+
+interface NavbarProps {
+	text: string;
+}
---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Navbar from '../components/navbar';
+
+export default function Home() {
+	return (
+		<div>
+			<h1>home</h1>
+			<Navbar text="banana" />
+		</div>
+	);
+};
