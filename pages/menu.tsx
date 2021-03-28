6500983325a5c0be4decea1131f308cc970ca3e1
flaskur <flaskur@gmail.com>
2021-03-28 19:47:44 -0400

    rendering menu from backend request

--- pages/menu.tsx
+++ pages/menu.tsx
@@ -1,9 +1,29 @@
-const Menu = () => {
+import { GetStaticProps } from "next";
+
+const Menu = ({message, menu}: any) => {
+	const renderMenu = () => {
+		console.log('called render');
+		return menu.map((food: any) => {
+			return <h3 key={food.food_id}>{food.name}</h3>
+		});
+	};
+	
 	return (
 		<div>
 			<h1>menu</h1>
+			<h2>{message}</h2>
+			{renderMenu()}
 		</div>
 	);
+};
+
+export const getStaticProps: GetStaticProps = async () => {
+	const result = await fetch('http://localhost:3001/menu');
+	const data = await result.json();
+
+	return {
+		props: data
+	};
 };
 
 export default Menu;
