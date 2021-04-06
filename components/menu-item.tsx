9f4e8c45f11226cb8361e76a5415c1be6bb846bf
flaskur <flaskur@gmail.com>
2021-04-06 10:33:23 -0400

    changed menu data structure to hash map for more logical component structure

--- components/menu-item.tsx
+++ components/menu-item.tsx
@@ -1,7 +1,50 @@
 import React from 'react';
 import MenuItemProps from '../shared/menu-item-props';
 
-const MenuItem = ({foodId, name, category, size, price, spicy, description}: MenuItemProps) => {
+const MenuItem = (menuItem: MenuItemProps) => {
+	let { foodId, name, category, size, price, spicy, description } = menuItem; // you need to change menuProps to include quantity and comments
+	
+	// cart item should not be exact as food item, you need to include quantity, etc.
+	// cart items only need foodId, quantity, and comment, but you should include name etc for checkout page rendering w/o asking backend to decipher foodId
+	const handleAddItem = () => {
+		console.log('handling item click');
+
+		// localstorage cart needs to start with {}
+
+		if (!localStorage.getItem('cart')) {
+			localStorage.setItem('cart', JSON.stringify([]));
+		}
+
+		let existingCart = JSON.parse(localStorage.getItem('cart')!);
+
+		// check if the particular food already exists, if it does increase quantity
+		// you can set a flag if quantity increase, so you don't trigger new object add
+
+		let foundDuplicateItem = false;
+		
+		let newCart = existingCart.map((cartItem: any) => {
+			if (cartItem.foodId === menuItem.foodId) {
+				// just increase the quantity
+				cartItem.quantity += 1;
+				foundDuplicateItem = true;
+			}
+
+			return cartItem;
+		});
+		
+		console.log('after checking quantity increase', newCart);
+
+		if (!foundDuplicateItem) {
+			newCart = [...existingCart, menuItem];
+		}
+		console.log('after adding non duplicate menu item', newCart);
+		
+		localStorage.setItem(
+			'cart',
+			JSON.stringify(newCart),
+		);
+	}
+	
 	return (
 		<div key={foodId}>
 			<p>{foodId}</p>
@@ -11,6 +54,7 @@ const MenuItem = ({foodId, name, category, size, price, spicy, description}: Men
 			<p>{price}</p>
 			<p>{spicy.toString()}</p>
 			<p>{description}</p>
+			<button onClick={handleAddItem}>Add Item</button>
 		</div>
 	);
 };
--- pages/menu.tsx
+++ pages/menu.tsx
@@ -4,14 +4,52 @@ import MenuItem from "../components/menu-item";
 import MenuItemProps from "../shared/menu-item-props";
 
 const Menu = ({message, menu}: any) => {
+	// NECESSARY TO COMBINE SIZE/PRICE FOR SAME NAME FOODS
+	const flattenMenu = () => {
+		const existingFood = new Set();
+		const newMenu = new Map();
+
+		for (let food of menu) {
+			let { foodId, name, category, size, price, spicy, description } = food;
+
+			// HAVE NOT ENCOUNTERED FOOD YET --> NEW ENTRY
+			if (!existingFood.has(food.name)) {
+				existingFood.add(food.name);
+
+				let newFood = {
+					category,
+					spicy,
+					description,
+					options: [{foodId, size, price}],
+				};
+				newMenu.set(name, newFood);
+			} else {
+				let prevFood = newMenu.get(name);
+				prevFood.options = [...prevFood.options, {foodId, size, price}]
+				newMenu.set(name, prevFood);
+			}
+			
+		}
+
+		return newMenu;
+	};
+
+	console.log('hello');
+	console.log(flattenMenu());
+	
+	const clearCart = () => {
+		localStorage.setItem('cart', JSON.stringify([]));
+	};
+	
 	const renderMenu = () => {
 		return menu.map((menuItem: MenuItemProps) => {
-			return <MenuItem {...menuItem} />;
+			return <MenuItem key={menuItem.foodId} {...menuItem} />;
 		});
 	};
 	
 	return (
 		<div>
+			<button onClick={clearCart}>Clear Cart</button>
 			<h2>{message}</h2>
 			{renderMenu()}
 		</div>
