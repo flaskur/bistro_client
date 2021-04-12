742e7dc05dcda839bc389aa71dc3a8ded9cebb15
flaskur <flaskur@gmail.com>
2021-04-11 23:28:31 -0400

    completed main logic to add cart items through local storage

--- components/menu-item-modal.tsx
+++ components/menu-item-modal.tsx
@@ -1,10 +1,67 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Option from '../shared/option';
 
 const MenuItemModal = ({showModal, closeModal, name, category, description, spicy, options}: MenuItemModalProps) => {
+	const [selectedOption, setSelectedOption] = useState<null | Option>(null);
+	const [quantity, setQuantity] = useState<number>(1); // need to convert input to number dropdown to disallow null/emptystring
+	const [comment, setComment] = useState('');
+	
+	// each cart item should be an object that has at least foodId, quantity, and comment, but you should include as much as possible so that I can render the checkout page easier.
 	const handleAddItem = () => {
 		console.log('handle add item');
+		if (!selectedOption) {
+			console.log('must select an option first');
+			return;
+		}
+
+		if (!localStorage.getItem('cart')) {
+			localStorage.setItem('cart', JSON.stringify([]));
+		}
+
+		let existingCart = JSON.parse(localStorage.getItem('cart')!);
+
+		// ONLY INCREASE QUANTITY IF FOOD ID(SIZE/PRICE) AND COMMENTS MATCH
+		let foundDuplicateItem = false;
+		let newCart = existingCart.map((cartItem: any) => {
+			if (cartItem.foodId === selectedOption.foodId && cartItem.comment === comment) {
+				cartItem.quantity += quantity;
+				foundDuplicateItem = true;
+			}
+
+			return cartItem;
+		});
+
+		// ADD NEW CART ITEM TO CART
+		if (!foundDuplicateItem) {
+			const cartItem = {
+				name,
+				foodId: selectedOption.foodId,
+				price: selectedOption.price,
+				size: selectedOption.size,
+				quantity,
+				comment,
+			};
+			newCart = [...existingCart, cartItem];
+		}
+
+		console.log(newCart);
+		
+		localStorage.setItem(
+			'cart',
+			JSON.stringify(newCart),
+		);
+
+		// RESET SELECTIONS
+		setSelectedOption(null);
+		setQuantity(1);
+		setComment('');
+		
 		closeModal();
+	};
+
+	const handleOptionSelect = (option: Option) => {
+		setSelectedOption(option);
+		console.log('selected option is', option.foodId, option.price, option.size);
 	};
 	
 	return (
@@ -23,8 +80,11 @@ const MenuItemModal = ({showModal, closeModal, name, category, description, spic
 							options.map(option => {
 								const {foodId, price, size} = option;
 								return (
-									// need to be able to select particular option
-									<div key={foodId}>
+									<div 
+										key={foodId} 
+										onClick={() => handleOptionSelect(option)} 
+										style={{backgroundColor: selectedOption && selectedOption.foodId === foodId ? 'lightgray' : 'white'}}
+									>
 										<p>{foodId}</p>
 										<p>{price}</p>
 										<p>{size}</p>
@@ -34,7 +94,10 @@ const MenuItemModal = ({showModal, closeModal, name, category, description, spic
 						}
 					</div>
 
-					<input type="number" />
+					{/* convert to number dropdown */}
+					<input type="number" value={quantity} min={1} max={10} onChange={(e) => {setQuantity(parseInt(e.target.value))}}/>
+
+					<input type="text" value={comment} onChange={(e) => setComment(e.target.value)} />
 
 					<button onClick={handleAddItem}>Add To Cart</button>
 				</div>
--- components/menu-item.tsx
+++ components/menu-item.tsx
@@ -8,49 +8,6 @@ const MenuItem = (menuItem: MenuItemProps) => {
 	
 	let { name, category, spicy, description, options } = menuItem; 
 	
-	// cart item should not be exact as food item, you need to include quantity, etc.
-	// cart items only need foodId, quantity, and comment, but you should include name etc for checkout page rendering w/o asking backend to decipher foodId
-	// this logic should be in menuitemmodal component
-	// const handleAddItem = () => {
-	// 	console.log('handling item click');
-
-	// 	// IF EMPTY, SET TO EMPTY ARRAY
-	// 	if (!localStorage.getItem('cart')) {
-	// 		localStorage.setItem('cart', JSON.stringify([]));
-	// 	}
-
-	// 	let existingCart = JSON.parse(localStorage.getItem('cart')!);
-
-	// 	// check if the particular food already exists, if it does increase quantity
-	// 	// you can set a flag if quantity increase, so you don't trigger new object add
-
-	// 	let foundDuplicateItem = false;
-		
-	// 	let newCart = existingCart.map((cartItem: any) => {
-	// 		if (cartItem.foodId === menuItem.foodId) {
-	// 			// just increase the quantity
-	// 			cartItem.quantity += 1;
-	// 			foundDuplicateItem = true;
-	// 		}
-
-	// 		return cartItem;
-	// 	});
-		
-	// 	console.log('after checking quantity increase', newCart);
-
-	// 	if (!foundDuplicateItem) {
-	// 		newCart = [...existingCart, menuItem];
-	// 	}
-	// 	console.log('after adding non duplicate menu item', newCart);
-		
-	// 	localStorage.setItem(
-	// 		'cart',
-	// 		JSON.stringify(newCart),
-	// 	);
-	// }
-
-	// does this generate a new component or just hide an existing modal?
-	// they generate it but have a state value for show, then change the css based on that.
 	const openModal = () => {
 		console.log('should open a modal element');
 		for (let option of menuItem.options) {
